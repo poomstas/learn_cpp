949cf22b1eec638d6e2897b55e82cbd00066fe43
brian <b.lee@utexas.edu>
2023-08-31 10:28:14 +0000

    for loop example

---
+++ for_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int elapsed_time = 15;
+    for (int i = 0; i < elapsed_time; i++) {
+        std::cout << "Traffic Light is Red" << std::endl;
+    }
+
+    std::cout << "Traffic Light is Green" << std::endl;
+
+    return 0;
+}
