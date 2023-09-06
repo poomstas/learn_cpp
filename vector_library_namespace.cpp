45b33a24e09e3caa9801fc3d07b898bfe7bf7fb9
brian <b.lee@utexas.edu>
2023-09-06 16:41:15 +0000

    sample for vector library use + namespace

---
+++ vector_library_namespace.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector> // Like Python's lists. Standard library
+
+using namespace std; // std:: can be omitted using this.
+
+int main() {
+    vector<int> intvectorvariable;
+    int intvariable = 5;
+    cout << intvariable << endl;
+    return 0;
+}
