1d8855aff8b0be53e586a97c9ddffb66496f3fda
brian <b.lee@utexas.edu>
2023-09-19 23:15:24 +0000

    added type enforcing

--- templates.cpp
+++ templates.cpp
@@ -10,4 +10,9 @@ int main() {
     print(5);
     print("this is string");
     print(5.5f);
+
+    // Specify the function to take in specific types
+    print<int>(5); // take in integers
+    print<std::string>("specified to be string"); // take in strings
+    print<float>(5.5f); // take in floats
 }
