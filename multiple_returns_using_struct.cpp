6c6e1c3d5e857fda0124e37f2eee4996ee051839
brian <b.lee@utexas.edu>
2023-09-20 22:11:45 +0000

    example of using struct to return multiple outputs from a function

---
+++ multiple_returns_using_struct.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// C++ does not by default return multiple items.
+// Here's how to work around that limitation -> return a struct
+
+// Simple Structure
+struct {
+    int integer;
+    std::string string_output;
+} myStructure;
+
+// Named Structure
+struct MultipleReturns {
+    std::string StringOutput;
+    float FloatOutput;
+};
+
+// Function Definition
+MultipleReturns function_name(std::string sentence, float number) {
+    MultipleReturns output;
+    output.StringOutput = sentence + "..."; // Add new string at the end
+    output.FloatOutput = number + 0.0001;
+    return output;
+}
+
+int main() {
+    // Simple structure
+    myStructure.integer = 1;
+    myStructure.string_output = "String output to a simple structure";
+
+    // Named structure
+    MultipleReturns return_item; // Define named structure
+    return_item.StringOutput = "This is a stored string";
+    return_item.FloatOutput = 0.123;
+    cout << return_item.StringOutput << endl;
+    cout << return_item.FloatOutput << endl;
+    cout << endl;
+
+    // Use Function to return multiple outputs (and print results)
+    std::string input_string = "This is an example sentence";
+    float input_number = 0.123;
+    MultipleReturns out = function_name(input_string, input_number);
+    cout << out.StringOutput << endl;
+    cout << out.FloatOutput << endl;
+}
