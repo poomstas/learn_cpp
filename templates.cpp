122ad73f118a58aecd4a887b20a88b9daa97d1dd
brian <b.lee@utexas.edu>
2023-09-20 07:45:57 +0000

    modified commentsg

--- templates.cpp
+++ templates.cpp
@@ -12,7 +12,7 @@ int main() {
     print(5.5f);
 
     // Specify the function to take in specific types
-    print<int>(5); // take in integers
-    print<std::string>("specified to be string"); // take in strings
-    print<float>(5.5f); // take in floats
+    print<int>(5);                                  // take in integers
+    print<std::string>("specified to be string");   // take in strings
+    print<float>(5.5f);                             // take in floats
 }
